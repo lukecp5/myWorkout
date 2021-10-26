b6f01da72369b54184b0175b4d062630ee1ed8cd
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-26 14:42:39 -0400

    Refactor server.js - Add additional options to the Mongoose connection Rearrange middleware for optimization

--- server.js
+++ server.js
@@ -4,19 +4,28 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3000;
-const app = express();
 
 const db = require("./models");
 
-app.use(logger("dev"));
 
-app.use("/", routes);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+app.use(logger("dev"));
+
+app.use(routes);
 
 
 // Start the server
