532c450205a606f49af24828ca3b5fd0e633b76a
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-26 12:15:28 -0400

    Add mongojs import to api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,14 +2,15 @@ const router = require("express").Router();
 const path = require("path");
 const app = require(".");
 const mongoose = require("mongoose");
+const mongojs = require("mongojs");
 
 const db = require("../models");
 
-const workout = {
-      name: "Running",
-      type: "Cardio",
-      duration: 15,
-}
+// const workout = {
+// 	name: "Running",
+// 	type: "Cardio",
+// 	duration: 15,
+// };
 
 router.get("/workouts", (req, res) =>{
       db.Workout.find({})
