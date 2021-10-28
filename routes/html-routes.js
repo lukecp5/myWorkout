50f7ce910ec90f81440518822abeffbb01eac853
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-28 14:19:20 -0400

    Add comments to the HTML router that handles the requests of all HTML pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const path = require("path");
-
+	// + Root directory of the application displays the index.html file for the user
 	router.get("/", (req, res) => {
 		res.sendFile(path.join(__dirname, "../public/index.html"));
 	});
 
+	// + Displays the "Add Exercise" page to the user when they or the application requests it
 	router.get("/exercise", (req, res) => {
 		res.sendFile(path.join(__dirname, "../public/exercise.html"));
 	});
 
+	// + Displays the "Stats" page to the user when they or the application requests it
 	router.get("/stats", (req, res) => {
 		res.sendFile(path.join(__dirname, "../public/stats.html"));
 	});
