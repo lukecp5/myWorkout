b7776a49a0e1d6a8321cbfe77b20d273d6c58c91
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-28 14:19:41 -0400

    Add comments to main server file that explains what each part does

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
+// + Import routes folder to handle the routing of API and HTML endpoints
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 
-
+// + Connect to MongoDB Atlas if deployed to Heroku, otherwise connect to local MongoDB instance
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
