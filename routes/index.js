9df20f754f1cb75e505be84c8f08ee4656384150
Luke Poirrier <lukepoirrier5@gmail.com>
2021-10-28 14:18:50 -0400

    Add comments to the routes/ index.js file that explains how the router works

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
+// + Import express to be used for routing
 const express = require('express');
 
+// + Import the two routers(API & HTML)
 const htmlRoutes = require("./html-routes");
 const apiRoutes = require("./api-routes");
 
+// + Create an app instance so that we can use the express router
 const app = express();
 
+// + Any API routes that need to be handled by the router go to the apiRoutes file, all other requests go to the htmlRoutes file
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
+// + Export app so that the router extends throughout the entire application
 module.exports = app;
